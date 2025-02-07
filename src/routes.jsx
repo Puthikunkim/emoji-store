b3fc2d963f8bce1fa6b122231866a23aec025ab2
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-02-08 08:14:53 +1300

    Implemented initial routing between pages

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import App from './App.jsx'
+// import ErrorPage from './ErrorPage.jsx'
+// import Store from './Store.jsx'
+
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        // errorElement: <ErrorPage />,
+        // children: [
+        //     {path: 'store', element: <Store />},
+        // ],
+
+    },
+]
+
+export default routes
