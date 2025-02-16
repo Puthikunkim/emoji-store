528b3fc23b4aaba227f23d851fa60ad87b4429a8
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-02-17 11:58:46 +1300

    Feat: Implemented hero section on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,20 @@
+import heroImage from "../../assets/images/hero.jpg";
 import styles from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
     return (
         <div className={styles.home}>
-            <h1>Welcome to Emojis.</h1>
-            <p>Discover our vibrant collection of emojis, packed with expression and personality. Delivered straight from our creative vault to your screen!</p>
+        <div className={styles.hero}>
+            <img src={heroImage} alt="Hero" />
+            <div className={styles.overlay}>
+            <h1>Where emojis meet the real world</h1>
+            <button onClick={() => navigate("/store")}>Shop Now</button>
+            </div>
         </div>
-    )
+        </div>
+    );
 }
 
-export default Home
+export default Home;
