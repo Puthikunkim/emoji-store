783b981b3fc8d8c6cc4bed57a47ab2522642a4b5
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-02-17 12:18:29 +1300

    Fix: Some styling issues

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import heroImage from "../../assets/images/hero.jpg";
 import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
+import Card from "../Store/CardContainer/Card/Card";
+import ShopContext from "../../context";
+import { useContext } from "react";
 
 function Home() {
+    const { fruits } = useContext(ShopContext);
     const navigate = useNavigate();
     return (
         <div className={styles.home}>
@@ -11,6 +15,15 @@ function Home() {
             <div className={styles.overlay}>
             <h1>Where emojis meet the real world</h1>
             <button onClick={() => navigate("/store")}>Shop Now</button>
+            </div>
+        </div>
+        <div className={styles.middle}>
+            <h2>Best seller</h2>
+            <div className={styles.cardContainer}>
+                <Card product={fruits[0]}/>
+                <Card product={fruits[1]}/>
+                <Card product={fruits[2]}/>
+                <Card product={fruits[3]}/>
             </div>
         </div>
         </div>
