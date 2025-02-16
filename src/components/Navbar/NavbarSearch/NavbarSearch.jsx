831cd86dd75f78914d227af91d5ccee606a35f08
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-02-17 10:14:58 +1300

    Feat: Implemented functionality for pressing enter in search

--- src/components/Navbar/NavbarSearch/NavbarSearch.jsx
+++ src/components/Navbar/NavbarSearch/NavbarSearch.jsx
@@ -1,18 +1,25 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import ShopContext from "../../../context";
 import { BiSearch } from "react-icons/bi";
 import styles from "./NavbarSearch.module.css";
 
-
 function NavbarSearch() {
-    const { query, setQuery } = useContext(ShopContext);
+  const { query, setQuery } = useContext(ShopContext);
+  const navigate = useNavigate();
 
-    return (
-        <div className={styles.searchContainer}>
-            <BiSearch className={styles.searchIcon} />
-            <input type="text" className={styles.searchInput} placeholder="Search" value={query} onChange={(e) => setQuery(e.target.value)}/>
-        </div>
-    );
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      navigate("/store");
+    }
+  };
+
+  return (
+    <div className={styles.searchContainer}>
+      <BiSearch className={styles.searchIcon} />
+      <input type="text" className={styles.searchInput} placeholder="Search" value={query} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} />
+    </div>
+  );
 }
 
 export default NavbarSearch;
